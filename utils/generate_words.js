e6071348d968162d39816ab1d392aedc3205f9dd
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2020-08-18 13:29:17 -0300

    generate words encrypted and commands.txt file included.

--- utils/generate_words.js
+++ utils/generate_words.js
@@ -7,6 +7,32 @@
 
 import { wallet } from '@hathor/wallet-lib';
 
-const words = wallet.generateWalletWords()
+const crypto = require('crypto');
 
+const IV_LENGTH = 16; // For AES, this is always 16
+const ENCRYPTION_KEY = '01031978010319780103197801031978'
+let iv = crypto.randomBytes(IV_LENGTH);
+
+const words = wallet.generateWalletWords()
 console.log(words);
+
+var mykey = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY,iv);
+var mystr = mykey.update(words)
+
+mystr = Buffer.concat([mystr, mykey.final()]);
+
+var mycriptstr = (iv.toString('hex') + ':' + mystr.toString('hex'));
+
+console.log(mycriptstr);
+
+let textParts = mycriptstr.split(':');
+let div = Buffer.from(textParts.shift(), 'hex');
+let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), div);
+let decrypted = decipher.update(encryptedText);
+
+decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+console.log(decrypted.toString());
+
+
